135ebdbabdacf8b93602ba13941aedd135112a34
enesdikmen <enesdikmen45@hotmail.com>
2021-04-18 21:54:21 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var button = document.getElementById("button");
+
+addEventListener("click", button =>{
+    let heading = document.getElementById("head");
+
+    heading.innerHTML = "Yeni"
+});
